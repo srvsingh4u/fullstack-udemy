41f3441b9c0fa1f34aabbefef65c8283baddd8f7
saurabh singh <saurabh.singh@appinessworld.com>
2022-04-29 14:08:47 +0530

    section [16] JavaScript Decison Making

---
+++ Section16/index.js
@@ -0,0 +1,28 @@
+console.log(2 > 3);
+console.log(3 < 4);
+console.log(4 >= 2);
+console.log(5 >= 5);
+console.log(1 == "1");
+console.log(1 === "1");
+console.log(1 != "1");
+console.log(1 !== "1");
+
+// alert("hy guys wassup");
+
+// const favNum = prompt("what si your fav number");
+// console.log(favNum);
+// document.write("hy guys ");
+const age = 14;
+
+if (age >= 18) {
+  console.log("Raj can drink wine");
+} else {
+  console.log("Raj has to drink water");
+}
+
+const star = 2;
+if (star === 3) {
+  console.log("The guys is a superstar");
+} else {
+  console.log("The guy is dull in his studies");
+}
