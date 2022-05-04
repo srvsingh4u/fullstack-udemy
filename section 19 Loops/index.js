cb5f45501b96f9334ae2602a0e6407e471bd07cb
saurabh singh <saurabh.singh@appinessworld.com>
2022-05-04 10:59:15 +0530

    SECTION 19 REPETING STUFFS WITH LOOP

--- section 19 Loops/index.js
+++ section 19 Loops/index.js
@@ -70,3 +70,109 @@
 //     console.log(i);
 //   }
 // }
+
+// let num = 0;
+// while (num < 10) {
+//   console.log(num);
+//   num++;
+// }
+
+// const secret = "babyHippo";
+// // let guess = prompt("eneter the animal name?");
+// while (guess !== secret) {
+//   guess = prompt("eneter the animal name?");
+// }
+// console.log("congratzzz u gussed it right");
+
+// let submit = prompt("Enter Anything U wish and I will replay back");
+// while (true) {
+//   submit = prompt(submit);
+//   if (submit === "stop it") {
+//     break;
+//   }
+// }
+// console.log("hi you won");
+
+// let maximum = parseInt(prompt("Guess the maximum number"));
+// while (!maximum) {
+//   maximum = parseInt(prompt("Guess the  maximum number"));
+// }
+
+// let targetNum = Math.floor(Math.random() * maximum) + 1;
+// console.log(targetNum);
+// let guess = parseInt(prompt("Enter your first guess"));
+
+// while (parseInt(guess) !== targetNum) {
+//   if (guess === "q") {
+//     break;
+//   }
+//   if (guess > targetNum) {
+//     guess = prompt("The number is high");
+//   } else {
+//     guess = prompt("The Number is low");
+//   }
+// }
+// if (guess === "q") {
+//   console.log("ok u quit");
+// } else {
+//   console.log("hy hy u guessed it right");
+// }
+
+// const age = 19;
+
+// const result = age < 18 ? " water " : "wine";
+// console.log(result);
+
+// const names = [
+//   "raj",
+//   "shiv",
+//   "amit",
+//   "raman",
+//   "abhi",
+//   "ram",
+//   "aryan",
+//   "malinga",
+// ];
+
+// for (const arr of names) {
+//   console.log(`HY my friend name is ${arr}`);
+// }
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// // WRITE YOUR LOOP BELOW THIS LINE:
+// for (const num of numbers) {
+//   console.log(num * num);
+// }
+
+// while (i < numbers.length) {
+//   console.log(i * i);
+// }
+const testScores = {
+  keenan: 80,
+  damon: 67,
+  kim: 89,
+  shawn: 91,
+  marlon: 72,
+  dwayne: 77,
+  nadia: 83,
+  elvira: 97,
+  diedre: 81,
+  vonnie: 60,
+};
+// const keys = Object.keys(testScores);
+// console.log(keys);
+
+// for (const k of keys) {
+//   console.log(k);
+// }
+
+const values = Object.values(testScores);
+console.log(values);
+let total = 0;
+for (const score of values) {
+  total = score + total;
+}
+console.log(total);
+const average = total / values.length;
+console.log(average);
