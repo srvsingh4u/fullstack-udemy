d8b83c90f17d35000afa6af22d657781159c6a59
saurabh singh <103627829+srvsingh4u@users.noreply.github.com>
2022-06-03 11:10:46 +0530

    section 40

--- Middleware/index.js
+++ Middleware/index.js
@@ -1,22 +1,37 @@
 const express = require("express");
+const { status } = require("express/lib/response");
 const app = express();
 const morgan = require("morgan");
+const apperror = require("./apperror");
 app.use(morgan("tiny"));
 
-app.use((req, res, next) => {
-  console.log("this is my first middleware");
-  next();
-});
-app.use((req, res, next) => {
-  console.log("this is my second middleware");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("this is my first middleware");
+//   next();
+// });
+// app.use((req, res, next) => {
+//   console.log("this is my second middleware");
+//   next();
+// });
 
 app.get("/", (req, res) => {
   res.send("hello its working ");
 });
 app.get("/dogs", (req, res) => {
   res.send("hy hy hy ");
+});
+app.get("/error", (req, res) => {
+  fly.chichken();
+});
+app.get("/admin", (res, req) => {
+  throw new apperror("you are not admin", 403);
+});
+app.use((err, req, res, next) => {
+  // console.log("*************************");
+  // console.log("************ERROR*************");
+  // console.log("*************************");
+  const { status = 500, message = "something went wrong" } = err;
+  res.status(status).send(message);
 });
 
 app.listen(3000, () => {
